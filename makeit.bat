1ba854feeffa2eddea02d2119be399333e0082d8
Pedro Pena <pedro.pena@noaa.gov>
2016-04-04 11:16:25 -0400

    added makeit.bat to compile on windows

---
+++ makeit.bat
@@ -0,0 +1,17 @@
+@echo off
+
+javac -source 1.6 -target 1.6 -d .\ .\src\binfileutils\*.java
+jar cfm AmverseasBinDecoder.jar manifest.txt binfileutils\*.class
+IF EXIST .\lib goto deletelib
+
+:deletelib
+del /q /s .\lib  > nul
+rmdir /q /s .\lib  > nul
+:exit
+
+mkdir .\lib
+move /y AmverseasBinDecoder.jar .\lib > nul
+del /s /q .\binfileutils > nul
+rmdir .\binfileutils > nul
+
+javadoc -author -version -use -d .\docs .\src\binfileutils\AmverseasBinDecoder.java
